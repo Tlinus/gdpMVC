2261f0ba7585b0d16f61669a9fc58ec07f3be17e
Tlinus <lemoingthibault@gmail.com>
2016-04-08 23:53:18 +0200

    update ajax

--- Ajax/select_user_appo_AJAX.php
+++ Ajax/select_user_appo_AJAX.php
@@ -3,11 +3,11 @@
 	session_start();
 	require './zOthers/BDD/connectbdd.php';
 
-	
+
 	$query= " SELECT nom, prenom FROM utilisateur where nom LIKE :name OR prenom LIKE :prenom";
 	$select_query = $bdd->prepare($query);
-	$select_query->bindValue(':name', 	$_POST['catch'], PDO::PARAM_STR);
-	$select_query->bindValue(':prenom', $_POST['catch'], PDO::PARAM_STR);
+	$select_query->bindValue(':name', 	'%'.$_POST['catch'].'%', PDO::PARAM_STR);
+	$select_query->bindValue(':prenom', '%'.$_POST['catch'].'%', PDO::PARAM_STR);
 	$select_query->execute();
 	$result = $select_query->fetchAll();
 	echo $result;
